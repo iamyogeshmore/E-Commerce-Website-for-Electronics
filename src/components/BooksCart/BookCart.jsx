fd4cb2d0e828c798158d2d70f5ac6e342930897f
Yogesh More <135803626+iamyogeshmore@users.noreply.github.com>
2023-09-10 00:18:38 +0530

    Added: Readme file updated.

--- src/components/BooksCart/BookCart.jsx
+++ src/components/BooksCart/BookCart.jsx
@@ -11,124 +11,124 @@ import emptyCartImg from '../../assets/empty_cart.png';
 
 const BookCart = () => {
 
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [cartBooks, setCartBooks] = useState([])
-    const [totalCartAmount, setTotalCartAmount] = useState(0)
-
-
-    useEffect(() => {
-      document.title = 'Cart';
-      fetchCartData();
-    }, [])
-
-    const fetchCartData = () => {
-      axios.get(`http://localhost:8083/CartPage/Show_Cart_Record?token=${localStorage.getItem("AccessToken")}`)
-      .then((res) => {
-        console.log(res.data.obj)
-        setCartBooks(res.data.obj);
-      })
-      .catch((err) => {
-        console.log(err.response.data)
-      })
-      console.log(cartBooks.length)
-    }
+  const [cartProducts, setCartProducts] = useState([])
+  const [totalCartAmount, setTotalCartAmount] = useState(0)
 
 
-    const increaseBookQty= (bookId) => {
-      axios.put(`http://localhost:8083/CartPage/IncreaseBookQty?bookId=${bookId}&token=${localStorage.getItem("AccessToken")}`)
-      .then((res) => {
-        toast.success(res.data.message, {position: toast.POSITION.BOTTOM_CENTER} );
-        console.log(res.data)
-        fetchCartData();
-      })
-      .catch((err) => {
-        toast.error(err.response.data);
-    });
-    }
+  useEffect(() => {
+    document.title = 'Cart';
+    fetchCartData();
+  }, [])
 
-    const decreaseBookQty= (bookId) => {
-      axios.put(`http://localhost:8083/CartPage/DecreaseBookQty?bookId=${bookId}&token=${localStorage.getItem("AccessToken")}`)
-      .then((res) => {
-        toast.success(res.data.message, {position: toast.POSITION.BOTTOM_CENTER});
-        fetchCartData();
-      })
-      .catch((err) => {
-        toast.error(err.response.data);
-    });
-    }
-
-    const removeBookFromCart=(id) => {
-      axios.delete(`http://localhost:8083/CartPage/Remove_Book_From_Cart?cartBookId=${id}&token=${localStorage.getItem("AccessToken")}`)
-      .then((res) => {
-        console.log(res.data);
-        toast.success(res.data.message, {position: toast.POSITION.BOTTOM_CENTER});
-        fetchCartData();
+  const fetchCartData = () => {
+    axios.get('https://fakestoreapi.com/carts/user/1')
+    .then((res) => {
+      console.log(res)
+      setCartProducts(res.data);
     })
     .catch((err) => {
-        console.log(err);
-    });
-    }
+      console.log(err.response.data)
+    })
+    console.log(cartProducts.length)
+  }
 
 
-  return (
-    <div>
-      <Header/>
-      <div className='cartContainer'>
-        {cartBooks.length>0 ? (cartBooks.map((cartBook) => {
-          console.log(cartBooks.length);
-          return (
-            <div className='cartcontainerbody'>
-              <Card key={cartBook.cartBookId} className='card' sx={{ display: 'flex', marginBottom: '1%', marginTop: '1%', width: '75%', maxHeight: '90%' }}>
-                <div>
-                  <CardMedia
-                    component="img"
-                    height="100px"
-                    image={`../../uploads/${cartBook.books.profilePic}`}
-                    alt="Image not Available"
-                    sx={{ objectFit: "contain", width: '150px' }} />
-                </div>
-                <div className='cardContent' >
-                  <CardContent class="cardcontent">
-                    <label className='cardtitle'>
-                        {cartBook.books.bookName}
-                    </label><br />
+  const increaseBookQty= (CartID) => {
+    axios.put(`http://localhost:8083/CartPage/IncreaseBookQty?CartID=${CartID}&token=${localStorage.getItem("Token")}`)
+    .then((res) => {
+      toast.success(res.data.message, {position: toast.POSITION.BOTTOM_CENTER} );
+      console.log(res.data)
+      fetchCartData();
+    })
+    .catch((err) => {
+      toast.error(err.response.data);
+  });
+  }
 
-                    <label className='authorname'>
-                        {cartBook.books.authorName}
-                    </label><br />
+  const decreaseBookQty= (CartID) => {
+    axios.put(`http://localhost:8083/CartPage/DecreaseBookQty?CartID=${CartID}&token=${localStorage.getItem("Token")}`)
+    .then((res) => {
+      toast.success(res.data.message, {position: toast.POSITION.BOTTOM_CENTER});
+      fetchCartData();
+    })
+    .catch((err) => {
+      toast.error(err.response.data);
+  });
+  }
 
-                    <label className='cardtitle'>
-                        Rs. {cartBook.totalPrice}
-                    </label><br /> <br />
+  const removeBookFromCart=(id) => {
+    axios.delete(`http://localhost:8083/CartPage/Remove_Book_From_Cart?id=${id}&token=${localStorage.getItem("Token")}`)
+    .then((res) => {
+      console.log(res.data);
+      toast.success(res.data.message, {position: toast.POSITION.BOTTOM_CENTER});
+      fetchCartData();
+  })
+  .catch((err) => {
+      console.log(err);
+  });
+  }
 
-                    <div className='countOfItems'style={{ marginTop: '1px' }}  >
-                      <button onClick={() => decreaseBookQty(cartBook.books.bookId)} disabled={cartBook.quantity === 1}style={{ marginRight: '10px' }}> - </button>
-                      <input  value={cartBook.quantity} className="count" type="text" name="countOfBook" id="Name" required />
-                      <button onClick={() => increaseBookQty(cartBook.books.bookId)}style={{ marginLeft: '10px' }}> + </button>
-                    </div>
-                  </CardContent>
-                </div>
-                <div className='cardAction'>
-                  <CardActions>
-                    <Button onClick={() => removeBookFromCart(cartBook.cartBookId)} variant="outlined" startIcon={<DeleteIcon />}>
-                        Remove
-                    </Button>
-                  </CardActions>
-                </div>
-              </Card>
-            </div>
-          );
-        })):(<img src={emptyCartImg} className="emptyCartImg"/>)
-      }{cartBooks.length>0 &&
-      <button class="place-order" onClick = {() => { navigate("/PlaceOrder") }}>
-        Place Order
-      </button> 
-      }      
-      </div>
-      <ToastContainer autoClose={2000} />  
+
+return (
+  <div>
+    <Header/>
+    <div className='cartContainer'>
+      {cartProducts.length>0 ? (cartProducts.map((cartProduct) => {
+        console.log(cartProducts.length);
+        return (
+          <div className='cartcontainerbody'>
+            <Card key={cartProduct.id} className='card' sx={{ display: 'flex', marginBottom: '1%', marginTop: '1%', width: '75%', maxHeight: '90%' }}>
+              <div>
+                <CardMedia
+                  component="img"
+                  height="100px"
+                  image={cartProduct.products[0].image}
+                  alt="Image not Available"
+                  sx={{ objectFit: "contain", width: '150px' }} />
+              </div>
+              <div className='cardContent'>
+                <CardContent class="cardcontent">
+                  <label className='cardtitle'>
+                      {cartProduct.products[0].title}
+                  </label><br />
+
+                  <label className='authorname'>
+                      {cartProduct.products[0].description}
+                  </label><br />
+
+                  <label className='cardtitle'>
+                      Rs. {cartProduct.products[0].price}
+                  </label><br />
+
+                  <div className='countOfItems'>
+                    <button onClick={() => decreaseBookQty(cartProduct.id, )} disabled={cartProduct.products[0].image === 1}> - </button>
+                    <input  value={cartProduct.products[0].image} className="count" type="text" name="countOfBook" id="Name" required />
+                    <button onClick={() => increaseBookQty(cartProduct.id)}> + </button>
+                  </div>
+                </CardContent>
+              </div>
+              <div className='cardAction'>
+                <CardActions>
+                  <Button onClick={() => removeBookFromCart(cartProduct.id)} variant="outlined" startIcon={<DeleteIcon />}>
+                      Remove
+                  </Button>
+                </CardActions>
+              </div>
+            </Card>
+          </div>
+        );
+      })):(<img src={emptyCartImg} className="emptyCartImg"/>)
+    }{cartProducts.length>0 &&
+    <button class="place-order" onClick = {() => { navigate("/PlaceOrder") }}>
+      Place Order
+    </button> 
+    }      
     </div>
-  )
+    <ToastContainer autoClose={2000} />  
+    </div>
+  )
 }
 
 export default BookCart

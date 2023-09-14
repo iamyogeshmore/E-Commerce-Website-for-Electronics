5ce81547a4990623ca26dbc997af99665d42eff3
Yogesh More <135803626+iamyogeshmore@users.noreply.github.com>
2023-09-14 19:16:45 +0530

    Added: cart login updated.

--- src/components/HomePage/Product.jsx
+++ src/components/HomePage/Product.jsx
@@ -18,12 +18,13 @@ const ProductDetails = () => {
   const [product, setProduct] = useState(null);
   const [productCount, setProductCount] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
-  const [selectedRAM, setSelectedRAM] = useState("4GB"); 
+  const [selectedRAM, setSelectedRAM] = useState("4GB");
   const [ramPrice, setRamPrice] = useState(0);
-  const [displayedImage, setDisplayedImage] = useState(null); 
+  const [displayedImage, setDisplayedImage] = useState(null);
   const [selectedStorage, setSelectedStorage] = useState("64GB");
   const [storagePrice, setStoragePrice] = useState(0);
-    const [isShown, setIsShown] = useState(false);
+  const [isShown, setIsShown] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
 
   useEffect(() => {
     document.title = "Product Details";
@@ -32,9 +33,10 @@ const ProductDetails = () => {
 
   useEffect(() => {
     if (product) {
-      setTotalPrice(product.price * productCount + ramPrice);
+      const updatedProductPrice = product.price + ramPrice + storagePrice;
+      setTotalPrice(updatedProductPrice * productCount);
     }
-  }, [product, productCount, ramPrice]);
+  }, [product, productCount, ramPrice, storagePrice]);
 
   const fetchProductDetails = (productId) => {
     fetch(`https://fakestoreapi.com/products/${productId}`)
@@ -49,27 +51,32 @@ const ProductDetails = () => {
   };
 
   const addToCart = async (id, quantity) => {
-    try {
-      const response = await axios.post("https://fakestoreapi.com/carts", {
-        productId: id,
-        quantity: quantity,
-      });
+    if (userLoggedIn) {
+      try {
+        const response = await axios.post("https://fakestoreapi.com/carts", {
+          productId: id,
+          quantity: quantity,
+        });
 
-      if (response.status === 200) {
-        toast.success("Product added to cart!");
-      } else {
-        toast.error("Failed to add product to cart. Please try again later.");
+        if (response.status === 200) {
+          toast.success("Product added to cart!");
+        } else {
+          toast.error("Failed to add product to cart. Please try again later.");
+        }
+      } catch (error) {
+        console.error("Error adding product to cart:", error);
+        toast.error("An error occurred while adding the product to the cart.");
       }
-    } catch (error) {
-      console.error("Error adding product to cart:", error);
-      toast.error("An error occurred while adding the product to the cart.");
+    } else {
+      toast.error("Please log to buy product.");
     }
   };
 
   const handleRAMOptionChange = (option, price) => {
     setSelectedRAM(option);
     setRamPrice(price);
-    setTotalPrice(product.price * productCount + price); // Update total price
+    const updatedProductPrice = product.price + price + storagePrice;
+    setTotalPrice(updatedProductPrice * productCount);
   };
 
   const randomImages = [
@@ -83,154 +90,153 @@ const ProductDetails = () => {
   const handleStorageOptionChange = (option, price) => {
     setSelectedStorage(option);
     setStoragePrice(price);
-    setTotalPrice(product.price * productCount + ramPrice + price); // Update total price
-};
+    const updatedProductPrice = product.price + ramPrice + price;
+    setTotalPrice(updatedProductPrice * productCount);
+  };
 
   return (
     <div>
-    <Header />
-    {product ? (
-      <div className="product-details">
-        <div className="random-images-container">
-          <h3>Random Images</h3>
-          <div className="random-images">
-            {randomImages.map((imageUrl, index) => (
-              <img
-                key={index}
-                src={imageUrl}
-                alt={`Random Image ${index}`}
-                className={`random-image ${
-                  imageUrl === displayedImage ? "selected" : ""
-                }`}
-                onClick={() => handleImageClick(imageUrl)}
-              />
-            ))}
+      <Header />
+      {product ? (
+        <div className="product-details">
+          <div className="random-images-container">
+            <h3>Random Images</h3>
+            <div className="random-images">
+              {randomImages.map((imageUrl, index) => (
+                <img
+                  key={index}
+                  src={imageUrl}
+                  alt={`Random Image ${index}`}
+                  className={`random-image ${
+                    imageUrl === displayedImage ? "selected" : ""
+                  }`}
+                  onClick={() => handleImageClick(imageUrl)}
+                />
+              ))}
+            </div>
           </div>
-        </div>
- 
 
-  
-        <div className="image-container"
-             onMouseEnter={() => setIsShown(true)}
-             onMouseLeave={() => setIsShown(false)}>
-        
-          <Card className="card" sx={{ maxWidth: 400 }}>
-            <CardMedia
-              component="img"
-              height="400"
-              image={displayedImage}
-              alt="Product Image"
-              sx={{ objectFit: "contain" }}
-            />
-          </Card>
-        </div>
+          <div
+            className="image-container"
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+          >
+            <Card className="card" sx={{ maxWidth: 400 }}>
+              <CardMedia
+                component="img"
+                height="400"
+                image={displayedImage}
+                alt="Product Image"
+                sx={{ objectFit: "contain" }}
+              />
+            </Card>
+          </div>
 
+          {isShown && (
+            <div className="hoverImage">
+              <Card sx={{ maxHeight: 1000 }}>
+                <CardMedia
+                  component="img"
+                  height="400"
+                  image={displayedImage}
+                  alt="Product Image"
+                  sx={{ objectFit: "contain" }}
+                />
+              </Card>
+            </div>
+          )}
 
-        {isShown && (
-        <div className="hoverImage"> 
-       <Card  sx={{ maxHeight: 1000 }}>
-            <CardMedia
-              component="img"
-              height="400"
-              image={displayedImage}
-              alt="Product Image"
-              sx={{ objectFit: "contain" }}
-            />
-          </Card>
+          {!isShown && (
+            <CardContent>
+              <h2 className="product-title">{product.title}</h2>
+              <p className="product-description">{product.description}</p>
+              <div className="product-price">
+                <p>Price: Rs. {totalPrice}</p>
+              </div>
+
+              <div className="ram-options">
+                <p>Select RAM:</p>
+                <div>
+                  <button
+                    className={selectedRAM === "4GB" ? "selected" : ""}
+                    onClick={() => handleRAMOptionChange("4GB", 0)}
+                  >
+                    4GB
+                  </button>
+                  <button
+                    className={selectedRAM === "8GB" ? "selected" : ""}
+                    onClick={() => handleRAMOptionChange("8GB", 50)}
+                  >
+                    8GB
+                  </button>
+                </div>
+              </div>
+              <div className="storage-options">
+                <p>Select Storage:</p>
+                <div>
+                  <button
+                    className={selectedStorage === "64GB" ? "selected" : ""}
+                    onClick={() => handleStorageOptionChange("64GB", 0)}
+                  >
+                    64GB
+                  </button>
+                  <button
+                    className={selectedStorage === "128GB" ? "selected" : ""}
+                    onClick={() => handleStorageOptionChange("128GB", 100)}
+                  >
+                    128GB
+                  </button>
+                </div>
+              </div>
+            </CardContent>
+          )}
+          {!isShown && (
+            <CardActions>
+              <div className="action-buttons">
+                <div className="product-count">
+                  <button
+                    onClick={() => {
+                      if (productCount > 1) {
+                        setProductCount(productCount - 1);
+                      }
+                    }}
+                    disabled={productCount <= 1}
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    value={productCount}
+                    onChange={(e) => {
+                      const newCount = parseInt(e.target.value);
+                      setProductCount(newCount);
+                      const updatedProductPrice =
+                        product.price + ramPrice + storagePrice;
+                      setTotalPrice(updatedProductPrice * newCount);
+                    }}
+                  />
+                  <button onClick={() => setProductCount(productCount + 1)}>
+                    +
+                  </button>
+                </div>
+                <Button
+                  startIcon={<ShoppingCartRounded />}
+                  onClick={() => addToCart(product.id, productCount)}
+                  size="small"
+                  variant="contained"
+                >
+                  Add To Cart
+                </Button>
+              </div>
+            </CardActions>
+          )}
         </div>
+      ) : (
+        <p>Loading product details...</p>
       )}
-
-   {!isShown && (
-        <CardContent>
-          <h2 className="product-title">{product.title}</h2>
-          <p className="product-description">{product.description}</p>
-          <p className="product-price">Price: Rs. {product.price}</p>
-          <div className="ram-options">
-            <p>Select RAM:</p>
-            <div>
-              <button
-                className={selectedRAM === "4GB" ? "selected" : ""}
-                onClick={() => handleRAMOptionChange("4GB", 0)}
-              >
-                4GB
-              </button>
-              <button
-                className={selectedRAM === "8GB" ? "selected" : ""}
-                onClick={() => handleRAMOptionChange("8GB", 50)}
-              >
-                8GB
-              </button>
-            </div>
-          </div>
-          <div className="storage-options">
-            <p>Select Storage:</p>
-            <div>
-              <button
-                className={selectedStorage === "64GB" ? "selected" : ""}
-                onClick={() => handleStorageOptionChange("64GB", 0)}
-              >
-                64GB
-              </button>
-              <button
-                className={selectedStorage === "128GB" ? "selected" : ""}
-                onClick={() => handleStorageOptionChange("128GB", 100)}
-              >
-                128GB
-              </button>
-            </div>
-          </div>
-        </CardContent>
-            ) }
-         {!isShown && (
-        <CardActions>
-          <div className="action-buttons">
-            <div className="product-count">
-              <button
-                onClick={() => {
-                  if (productCount > 1) {
-                    setProductCount(productCount - 1);
-                  }
-                }}
-                disabled={productCount <= 1}
-              >
-                -
-              </button>
-              <input
-                type="number"
-                value={productCount}
-                onChange={(e) => {
-                  const newCount = parseInt(e.target.value);
-                  setProductCount(newCount);
-                  setTotalPrice(
-                    product.price * newCount + ramPrice + storagePrice
-                  ); // Update total price
-                }}
-              />
-              <button onClick={() => setProductCount(productCount + 1)}>
-                +
-              </button>
-            </div>
-            <Button
-              startIcon={<ShoppingCartRounded />}
-              onClick={() => addToCart(product.id, productCount)}
-              size="small"
-              variant="contained"
-            >
-              Add To Cart
-            </Button>
-            <div className="total-price">
-              <p>Total Price: Rs. {totalPrice}</p>
-            </div>
-          </div>
-        </CardActions>
-        ) }
-      </div>
-    ) : (
-      <p>Loading product details...</p>
-    )}
-    <ToastContainer autoClose={2000} />
-  </div>
-);
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
 };
 
 export default ProductDetails;
